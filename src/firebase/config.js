418b54ac47d3af3757adc3111bc9a2ee31e1bc25
SirDev97 <alkin971@gmail.com>
2021-12-09 13:18:36 +0100

    init firebase storage

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,14 +1,15 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyCXNvlqf2tudGPnc3Pr6j5OzPL1tAyHAQ0',
-    authDomain: 'ourdojo-ad439.firebaseapp.com',
-    projectId: 'ourdojo-ad439',
-    storageBucket: 'ourdojo-ad439.appspot.com',
-    messagingSenderId: '308512696414',
-    appId: '1:308512696414:web:78df478b111a3f4c92eccf',
+  apiKey: 'AIzaSyCXNvlqf2tudGPnc3Pr6j5OzPL1tAyHAQ0',
+  authDomain: 'ourdojo-ad439.firebaseapp.com',
+  projectId: 'ourdojo-ad439',
+  storageBucket: 'ourdojo-ad439.appspot.com',
+  messagingSenderId: '308512696414',
+  appId: '1:308512696414:web:78df478b111a3f4c92eccf',
 };
 
 // init firebase
@@ -17,9 +18,10 @@ firebase.initializeApp(firebaseConfig);
 // init services
 const projectFirestore = firebase.firestore();
 const projectAuth = firebase.auth();
+const projectStorage = firebase.storage();
 
 // timestamp
 
 const timestamp = firebase.firestore.Timestamp;
 
-export { projectFirestore, projectAuth, timestamp };
+export { projectFirestore, projectAuth, projectStorage, timestamp };
