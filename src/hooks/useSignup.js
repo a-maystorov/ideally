a798599588f1aff97ec6c2730528f43bbc83f420
SirDev97 <alkin971@gmail.com>
2021-12-09 13:31:33 +0100

    update sign up hook using firebase storage to upload images

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,49 +1,52 @@
 import { useState, useEffect } from 'react';
-import { projectAuth } from '../firebase/config';
+import { projectAuth, projectStorage } from '../firebase/config';
 import { useAuthContext } from './useAuthContext';
 
 export const useSignup = () => {
-    const [isCancelled, setIsCancelled] = useState(false);
-    const [error, setError] = useState(null);
-    const [isPending, setIsPending] = useState(false);
-    const { dispatch } = useAuthContext();
+  const [isCancelled, setIsCancelled] = useState(false);
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
 
-    const signup = async(email, password, displayName) => {
+  const signup = async (email, password, displayName, thumbnail) => {
+    setError(null);
+    setIsPending(true);
+
+    try {
+      // signup
+      const res = await projectAuth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+
+      if (!res) throw new Error('Could not complete signup');
+
+      // upload user thumbnail
+      const uploadPath = `thumbnails/${res.user.uid}/${thumbnail.name}`;
+      const img = await projectStorage.ref(uploadPath).put(thumbnail);
+      const imgURL = await img.ref.getDownloadURL();
+
+      // add display name to user
+      await res.user.updateProfile({ displayName, photoURL: imgURL });
+
+      // dispatch login action
+      dispatch({ type: 'LOGIN', payload: res.user });
+
+      if (!isCancelled) {
+        setIsPending(false);
         setError(null);
-        setIsPending(true);
+      }
+    } catch (err) {
+      if (!isCancelled) {
+        setError(err.message);
+        setIsPending(false);
+      }
+    }
+  };
 
-        try {
-            // signup
-            const res = await projectAuth.createUserWithEmailAndPassword(
-                email,
-                password
-            );
+  useEffect(() => {
+    return () => setIsCancelled(true);
+  }, []);
 
-            if (!res) {
-                throw new Error('Could not complete signup');
-            }
-
-            // add display name to user
-            await res.user.updateProfile({ displayName });
-
-            // dispatch login action
-            dispatch({ type: 'LOGIN', payload: res.user });
-
-            if (!isCancelled) {
-                setIsPending(false);
-                setError(null);
-            }
-        } catch (err) {
-            if (!isCancelled) {
-                setError(err.message);
-                setIsPending(false);
-            }
-        }
-    };
-
-    useEffect(() => {
-        return () => setIsCancelled(true);
-    }, []);
-
-    return { signup, error, isPending };
+  return { signup, error, isPending };
 };
